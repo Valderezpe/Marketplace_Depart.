b518ae0b65bd9dae0706749178d3c0b40528f5cf
valderez <valderezpe@gmail.com>
2024-01-15 13:18:09 -0300

    feat: create connect DB

---
+++ src/main/java/com/valderez/br/marketplace_lojas/config/mongo/MongoDBConfig.java
@@ -0,0 +1,20 @@
+package com.valderez.br.marketplace_lojas.config.mongo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDBConfig {
+    @Bean
+    public MongoDatabaseFactory mongoConfig(){
+        return  new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/product-catelog");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(){
+        return new MongoTemplate(mongoConfig());
+    }
+}
